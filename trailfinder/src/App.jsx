a3343020e43e2799bec0fdaa7a0b72d1fa24009f
alan-w-u <alanwu0004@gmail.com>
2024-07-26 19:09:53 -0700

    Initial commit

---
+++ trailfinder/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div id="logo">
+          <img src="trailfinder.png" alt="TrailFinder" draggable="false" />
+          <h1>TrailFinder</h1>
+        </div>
+      </nav>
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
