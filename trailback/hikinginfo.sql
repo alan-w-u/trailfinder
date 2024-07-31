08dba018b082ad61ef1391f2e6596d291a9db3e4
dylz13 <xzhan185@student.ubc.ca>
2024-07-31 14:57:50 -0700

    sql statement touchups

--- trailback/hikinginfo.sql
+++ trailback/hikinginfo.sql
@@ -1,11 +1,11 @@
-drop table UserProfile3;
-drop table UserProfile1;
-drop table UserProfile5;
 drop table Equipment6;
-drop table Equipment1; 
-drop table UserProfile6;
 drop table Equipment5;
 drop table Equipment3;
+drop table Equipment1;
+drop table UserProfile6;
+drop table UserProfile5;
+drop table UserProfile3;
+drop table UserProfile1;
 
 
 CREATE TABLE UserProfile1 (
@@ -18,7 +18,7 @@ CREATE TABLE UserProfile3 (
 Email 			VARCHAR(320) 	PRIMARY KEY,
 ProfilePicture 	BLOB, 
 TrailsHiked 		INTEGER		DEFAULT 0,
-FOREIGN KEY (TrailsHiked) REFERENCES User1
+FOREIGN KEY (TrailsHiked) REFERENCES UserProfile1
 );
 
 grant select on UserProfile3 to public;
@@ -27,7 +27,8 @@ CREATE TABLE UserProfile5 (
 Name 			VARCHAR(50),
 Email 			VARCHAR(320), 
 NumberOfFriends	INTEGER		DEFAULT 0,
-PRIMARY KEY (Name, Email)
+PRIMARY KEY (Name, Email),
+FOREIGN KEY (Email) REFERENCES UserProfile3
 );
 
 grant select on UserProfile5 to public;
@@ -36,24 +37,15 @@ CREATE TABLE UserProfile6 (
 UserID 		INTEGER		PRIMARY KEY,
 Name 			VARCHAR(50),
 Email 			VARCHAR(320),
-FOREIGN KEY (Name, Email) REFERENCES User5
+FOREIGN KEY (Name, Email) REFERENCES UserProfile5
 );
 
 grant select on UserProfile6 to public;
 
-CREATE TABLE Equipment6 (
-Equipment 		INTEGER 		PRIMARY KEY,
-UserID			INTEGER,
-FOREIGN KEY (UserID) REFERENCES User6
-ON DELETE CASCADE
-);
-
-grant select on Equipment6 to public; 
-
 CREATE TABLE Equipment1 (
 UserID	 		INTEGER 		PRIMARY KEY,
 Amount 		INTEGER,
-FOREIGN KEY (UserID) REFERENCES User6
+FOREIGN KEY (UserID) REFERENCES UserProfile6
 ON DELETE CASCADE
 );
 
@@ -73,5 +65,13 @@ Weight 		FLOAT,
 FOREIGN KEY (EquipmentID) REFERENCES Equipment3
 );
 
-grant select on Equipment5 to public; 
+grant select on Equipment5 to public;
 
+CREATE TABLE Equipment6 (
+Equipment 		INTEGER 		PRIMARY KEY,
+UserID			INTEGER,
+FOREIGN KEY (UserID) REFERENCES UserProfile6
+    ON DELETE CASCADE
+);
+
+grant select on Equipment6 to public;
