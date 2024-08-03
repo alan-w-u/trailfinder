473a224e9911324933253461bb43a3cc51b61a70
alan-w-u <alanwu0004@gmail.com>
2024-08-02 17:21:41 -0700

    Update App file

--- trailfront/src/App.jsx
+++ trailfront/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Auth from './components/Auth.jsx'
 import TrailWidget from './components/TrailWidget.jsx'
 import './App.css'
-import {BrowserRouter as Router, Route, Routes, Navigate, Outlet} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import { AuthProvider, useAuth } from './components/AuthContext.jsx';
 
 import LoginPage from './pages/LoginPage.jsx'
@@ -13,63 +13,62 @@ import Navbar from "./components/Navbar.jsx";
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 function App() {
-  const [userID, setUserID] = useState(null);
-  const [status, setStatus] = useState('');
+    const [userID, setUserID] = useState(null);
+    const [status, setStatus] = useState('');
 
 
-  useEffect(() => {
-    async function checkConnection() {
-      try {
-        const response = await fetch('http://localhost:65535/check-db-connection', {
-          mode: "cors"
-        });
-        const text = await response.text();
-        setStatus(text);
-      } catch (err) {
-        setStatus('connection timed out');
-      }
-    }
+    useEffect(() => {
+        async function checkConnection() {
+            try {
+                const response = await fetch('http://localhost:65535/check-db-connection', {
+                    mode: "cors"
+                });
+                const text = await response.text();
+                setStatus(text);
+            } catch (err) {
+                setStatus('connection timed out');
+            }
+        }
 
-    checkConnection();
-  }, []);
+        checkConnection();
+    }, []);
 
-  useEffect(() => {
-    async function initializeDB() {
-      if (status) {
-        await fetch("http://localhost:65535/initialize", {
-          method: 'POST'
-        });
-      }
-    }
+    useEffect(() => {
+        async function initializeDB() {
+            if (status) {
+                await fetch("http://localhost:65535/initialize", {
+                    method: 'POST'
+                });
+            }
+        }
 
-    //use this code to initialize the DB on tunnel connection success
-    // initializeDB();
-  }, [status]);
+        initializeDB(); // Use this code to initialize the DB on tunnel connection success
+    }, [status]);
 
-  return (
-    <>
-      <AuthProvider>
-        <Router>
-          <Navbar status={status} />
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="/profile" element={<ProfilePage />} />
-            </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="/home" element={<HomePage />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </>
-  )
+    return (
+        <>
+            <AuthProvider>
+                <Router>
+                    <Navbar status={status} />
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/profile" element={<ProfilePage />} />
+                        </Route>
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/home" element={<HomePage />} />
+                        </Route>
+                        <Route path="*" element={<Navigate to="/login" replace />} />
+                    </Routes>
+                </Router>
+            </AuthProvider>
+        </>
+    )
 }
 
 export default App
